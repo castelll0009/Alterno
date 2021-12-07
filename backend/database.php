8da112986e2b96e4f58a0c43b951f4469760434c
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2021-12-06 19:16:48 -0500

    Agregue conexion  base de datos
    se agrego el archivo backend/database.php
     para la conexion a la base de  datos

---
+++ backend/database.php
@@ -0,0 +1,19 @@
+<?php //gestina las peticiones que se hacen desde el request-app.js 
+    //No conectamos a la base de datos
+    $connection = mysqli_connect(
+        //host,user,pasword,nombre base datos,
+        /* base de datos Clever cloud
+        'blzwwwafrtnoulyompah-mysql.services.clever-cloud.com', 'uflmxyisjbdu7w7l', 'k5ulb9EJWmdqIgYLjNHF', 'blzwwwafrtnoulyompah'
+        */
+        //conexion Base de datos hosgator
+        '162.241.61.135', 'mojitosc_castelll0009','Castillo1997177-','mojitosc_mojitos-db'
+    );
+    
+    /*
+    if($connection){
+        echo "database is connected";
+    }   else{
+        echo "no se pudo conectar a la BD";
+    }
+    */
+?>
