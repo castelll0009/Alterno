1977efaedec81899af8907bd35baa4559da1d47b
Arcaurus99 <32331814+Arcaurus99@users.noreply.github.com>
2021-12-08 02:26:56 -0500

    Js sin funcion eliminado

--- js/collapse-form.js
+++
@@ -1,6 +0,0 @@
-//$(document).on('click', '.product-item', function() {
-$("#collapse-btn").click(function(){
-    
-    document.getElementById('#collapse-btn').innerHTML = 'Ver menos...';
-          
-});
