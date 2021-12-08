df3bf5aa34bbfce7f609cad00688797919106161
Arcaurus99 <32331814+Arcaurus99@users.noreply.github.com>
2021-12-08 02:24:31 -0500

    Acciones más completas
    Funciones de Administración Productos:
    Añadidos los campos (Propiedades, Usos y Recetas) para añadir y editar.
    
    Observaciónes IMPORTANTES:
    - La tabla solo muestra una limitada cantidad de caracteres de parte de las descripciones, lo mas seguro es que se deba cambiar en la base de datos la cantidad de caracteres a almacenar.
    - Tipo de dato en Recetas, no imprime en la tabla. Verificar!
    
    Formulario - Función Collapse:
    Se añadio una funcion para ocultar/mostrar los datos adicionales y facilitar el trabajo con el formulario.
    
    Estilos:
    Algunos estilos cambiados en html index y admin.
    Logo añadido en la barra de navegación.

--- backend/product-edit.php
+++ backend/product-edit.php
@@ -6,13 +6,17 @@
         $name = $_POST['name'];
         $price = $_POST['price'];
         $description = $_POST['description'];
+        $properties = $_POST['properties'];
+        $uses = $_POST['uses'];
+        $recipes = $_POST['recipes'];        
 
-        $query = "UPDATE tbl_producto SET nombre = '$name', descripcion = '$description', precio = '$price' WHERE id = '$id'";
+        $query = "UPDATE tbl_producto SET nombre = '$name', descripcion = '$description', precio = '$price',  propiedades = '$properties', usos = '$uses', recetas = '$recipes' WHERE id = '$id'";
+
         $result = mysqli_query($connection, $query);
         if(!$result){
             die('Edit Query Failed');
         }
     }
 
-    echo "Update Task Successfully";
+    echo "Update Product Successfully";
 ?>
---
+++ js/collapse-form.js
@@ -0,0 +1,6 @@
+//$(document).on('click', '.product-item', function() {
+$("#collapse-btn").click(function(){
+    
+    document.getElementById('#collapse-btn').innerHTML = 'Ver menos...';
+          
+});
