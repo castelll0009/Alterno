8501da38eb0695d86bb7f18955d0ce86e0622772
Arcaurus99 <32331814+Arcaurus99@users.noreply.github.com>
2021-12-09 05:00:42 -0500

    Grafica - Valores X
    Se puede ver en consola la creación correcta de los valores de x con el formato apropiado.

--- js/graficas.js
+++ js/graficas.js
@@ -1,5 +1,6 @@
-//consulta primeros  5  , insertarlos
-var xValues = ["producto1", "Producto2", "Producto3", "Producto4", "Producto5"];
+
+var xValues;
+fetchProducts();
 //reemplazar por los  valores cantidad de ventas
 var yValues = [55, 49, 44, 24, 15];
 var barColors = ["red", "green","blue","orange","brown"];
@@ -21,3 +22,27 @@ new Chart("myChart", {
     }
   }
 });
+
+// Fetching Products
+function fetchProducts() {
+  $.ajax({
+    url: 'backend/products-list.php',
+    type: 'GET',
+    success: function(response) {
+      const products = JSON.parse(response);
+      xValues = '[';
+      let cont = 1;
+      console.log(products);
+      products.forEach(product => {
+        if(cont == 1){
+          xValues += `"${product.nombre}"`
+        } else {
+          xValues += `, "${product.nombre}"`
+        }
+        cont ++;
+      });
+      xValues += `]`
+      console.log(xValues);
+    }
+  });
+}
